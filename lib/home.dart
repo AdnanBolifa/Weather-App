41197f3ee1093ac6606e1ab757e5fce737b1fb8e
TheF0x <retr0f0x.f0@gmail.com>
2023-08-31 08:36:59 -0700

    http

--- lib/home.dart
+++ lib/home.dart
@@ -24,7 +24,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
     String cityName = 'London';
     final res = await http.get(
       Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?q=$cityName,uk&APPID=$openWeatherAPIKey'),
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName,uk&APPID=$openWeatherAPIKey'),
     );
     debugPrint(res.body);
   }
