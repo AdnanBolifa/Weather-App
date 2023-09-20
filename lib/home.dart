5a1bef5561e0d784540be1f4e3a51a9709bb65d3
TheF0x <retr0f0x.f0@gmail.com>
2023-09-19 22:58:36 -0700

    Finished! 100%

--- lib/home.dart
+++ lib/home.dart
@@ -16,12 +16,7 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  double temp = 0;
-  @override
-  void initState() {
-    super.initState();
-    getCurrentWeather();
-  }
+  late Future<Map<String, dynamic>> weather;
 
   Future<Map<String, dynamic>> getCurrentWeather() async {
     String cityName = 'Misratah';
@@ -42,6 +37,12 @@ class _WeatherScreenState extends State<WeatherScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    weather = getCurrentWeather();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -54,13 +55,15 @@ class _WeatherScreenState extends State<WeatherScreen> {
           IconButton(
             icon: const Icon(Icons.refresh),
             onPressed: () {
-              setState(() {});
+              setState(() {
+                weather = getCurrentWeather();
+              });
             },
           ),
         ],
       ),
       body: FutureBuilder(
-        future: getCurrentWeather(),
+        future: weather,
         builder: (context, snapshot) {
           debugPrint('snapshot $snapshot');
           if (snapshot.connectionState == ConnectionState.waiting) {
